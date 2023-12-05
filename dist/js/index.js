48be8a607c84349d03e564f1152dcca9687f7c35
Victor Silva <vitinhogsoo@gmail.com>
2023-12-05 11:34:16 -0300

    initial

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,99 +1,63 @@
+"use strict";
 // string, boolean, number, ...
-let x: number = 40;
+let x = 40;
 console.log(x);
-
-let firstName: string = "Victor";
-let age: number = 21;
-const isAdmin: boolean = true;
-
+let firstName = "Victor";
+let age = 21;
+const isAdmin = true;
 //String != string
-
 console.log(typeof firstName);
-
 firstName = "Pedrinho";
 console.log(firstName);
-
 // array numérico
-const myNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(myNumbers);
 console.log(myNumbers.length);
-
 //tuplas
-let myTuple: [number, string, string[]]
-myTuple = [5, "teste", ["olá", "mundo"]]
-
+let myTuple;
+myTuple = [5, "teste", ["olá", "mundo"]];
 console.log(myTuple);
-
 //object literals -> {prop: value}
-
-const user: { name: string, age: number } = {
+const user = {
     name: "Victor",
     age: 18
 };
-
 console.log(user);
-
-
-// type alias
-type myId = number | string
-const userId: myId = 10;
-const productId: myId = "1009";
-
-
+const userId = 10;
+const productId = "1009";
 // enum
-
-enum Size {
-    p = "Pequeno",
-    M = "Médio",
-    G = "Grande",
-    GG = "Extra Grande"
-}
-
+var Size;
+(function (Size) {
+    Size["p"] = "Pequeno";
+    Size["M"] = "M\u00E9dio";
+    Size["G"] = "Grande";
+    Size["GG"] = "Extra Grande";
+})(Size || (Size = {}));
 const blusa = {
     name: "Blusa verde",
     size: Size.M
-}
-
+};
 console.log(blusa);
-
 // literal types
-
-let auth: "autenticado" | null;
+let auth;
 auth = "autenticado";
 auth = null;
-
 // funções
 // função tipada
-function soma(a: number, b: number) {
+function soma(a, b) {
     return a + b;
 }
-
 console.log(soma(44, 13));
-
-
-function hello(msg: string): string {
+function hello(msg) {
     return `Hello ${msg}`;
 }
-
 console.log(hello("Victor Silva"));
-
 // função void
-function log(msg: string): void {
+function log(msg) {
     console.log(msg);
 }
-
 log("Testando função log");
-
-//interfaces
-
-interface sumNums {
-    n1: number,
-    n2: number
-}
-
-
-function sum(nums: sumNums) {
+function sum(nums) {
     return nums.n1 + nums.n2;
 }
-
 console.log(sum({ n1: 4, n2: 6 }));
